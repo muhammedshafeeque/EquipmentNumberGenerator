a70e470e1844ce4914e1386118845490f21bf4d4
Muhammed Shafeeque P <shafeequekkv95@gmail.com>
2023-06-11 18:31:40 +0530

    updatd

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,26 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
+  const navigate=useNavigate()
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Equipment Number Generator</Navbar.Brand>
+        <Navbar.Brand onClick={()=>{
+          navigate('/')
+        }}>Equipment Number Generator</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/excel">Excel Generate </Nav.Link>
-            <Nav.Link href="/sets">Sets</Nav.Link>
+            <Nav.Link onClick={()=>{
+          navigate('/excel')
+        }} >Excel Generate </Nav.Link>
+            <Nav.Link onClick={()=>{
+          navigate('/sets')
+        }}  >Sets</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
